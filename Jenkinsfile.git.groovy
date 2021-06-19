ba89aa169c174ee95a548ec428747d5e5b7f2318
trank <Kevin.Tran@pointclickcare.com>
2021-06-19 00:06:09 -0400

    Fix syntax

--- Jenkinsfile.git.groovy
+++ Jenkinsfile.git.groovy
@@ -1,17 +1,28 @@
 def GIT_CREDENTIALS_ID = 'global_credentials_ssh'
+def LATEST_TAG = "deploy-latest"
 
 pipeline {
     agent any
     parameters {
-        string(name: 'DEPLOY_COMMIT_ID', description: 'The Commit ID that we want to deploy', defaultValue: '')
+        string(name: 'DEPLOY_COMMIT_ID', defaultValue: '', description: 'The Commit ID that we want to deploy')
     }
     stages {
         stage('Checkout associated commit') {
             steps {
+                echo "DEPLOY_COMMIT_ID: ${params.DEPLOY_COMMIT_ID}"
                 checkout scm
                 script {
-                    sh "git checkout ${DEPLOY_COMMIT_ID}"
+                    sh "git checkout ${params.DEPLOY_COMMIT_ID}"
                 }
+            }
+        }
+    }
+    stage('Update latest tag') {
+        steps {
+            sshagent(credentials: [GIT_CREDENTIALS_ID]) {
+                sh "git tag --force ${LATEST_TAG}"
+                sh "git push origin --delete ${LATEST_TAG}"
+                sh "git push origin ${LATEST_TAG}"
             }
         }
     }
