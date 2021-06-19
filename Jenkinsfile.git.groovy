7747ed3f029e99f77a8b6d516220b4799ce137c0
trank <Kevin.Tran@pointclickcare.com>
2021-06-19 00:00:06 -0400

    Fix syntax

--- Jenkinsfile.git.groovy
+++ Jenkinsfile.git.groovy
@@ -5,11 +5,13 @@ pipeline {
     parameters {
         string(name: 'DEPLOY_COMMIT_ID', description: 'The Commit ID that we want to deploy', defaultValue: '')
     }
-    stage('Checkout associated commit') {
-        steps {
-            checkout scm
-            script {
-                sh "git checkout ${DEPLOY_COMMIT_ID}"
+    stages {
+        stage('Checkout associated commit') {
+            steps {
+                checkout scm
+                script {
+                    sh "git checkout ${DEPLOY_COMMIT_ID}"
+                }
             }
         }
     }
